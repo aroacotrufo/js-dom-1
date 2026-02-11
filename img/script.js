0d62ba043fbc0c4785e4c3df90766e14186bc9ef
Aroa Cotrufo <aroa.c97@gmail.com>
2026-02-11 19:19:53 +0100

    chore

--- img/script.js
+++ img/script.js
@@ -8,6 +8,7 @@ button.addEventListener('click',                        //quando clicco
             img.src = "./yellow_lamp.png"               //se sì, cambia la sorgente dell'immagine con l'altra (gialla)
             button.innerHTML ="Spegni"                  //cambia il testo all'interno del bottone
         }else{                                          //altrimenti, se non contiene il bianco, quindi è giallo
+            //bonus      
             img.src='./white_lamp.png'                  //cambia l'immagine in bianco(si spegne)
             button.innerHTML = "Accendi"                //cambia il testo in accendi
         }
