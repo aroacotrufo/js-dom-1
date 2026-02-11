8f76e8890cda719fc2efe936d692db81de5d088e
Aroa Cotrufo <aroa.c97@gmail.com>
2026-02-11 18:53:34 +0100

    aggiunto click su script

--- img/script.js
+++ img/script.js
@@ -1,1 +1,10 @@
 const button = document.getElementById ("bottone")
+const img = document.getElementById("spenta")
+const image = ['white_lamp.png','yellow_lamp.png']
+
+button.addEventListener('click',
+    
+    function (){
+    console.log('Acceso')
+    }
+)
