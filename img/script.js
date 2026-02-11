475bcc7d8927448f72732ca83abe53056823536c
Aroa Cotrufo <aroa.c97@gmail.com>
2026-02-11 19:15:38 +0100

    esercizio terminato con bonus

--- img/script.js
+++ img/script.js
@@ -1,10 +1,15 @@
-const button = document.getElementById ("bottone")
-const img = document.getElementById("spenta")
-const image = ['white_lamp.png','yellow_lamp.png']
+const button = document.getElementById ("bottone")      //riprendo gli elementi che voglio utilizzare
+const img = document.getElementById("spenta")           
 
-button.addEventListener('click',
-    
+button.addEventListener('click',                        //quando clicco
     function (){
-    console.log('Acceso')
+       // console.log('Acceso')
+        if(img.src.includes('white')){                  //controlla che l'attributo src dell'img contiene la parola white
+            img.src = "./yellow_lamp.png"               //se sì, cambia la sorgente dell'immagine con l'altra (gialla)
+            button.innerHTML ="Spegni"                  //cambia il testo all'interno del bottone
+        }else{                                          //altrimenti, se non contiene il bianco, quindi è giallo
+            img.src='./white_lamp.png'                  //cambia l'immagine in bianco(si spegne)
+            button.innerHTML = "Accendi"                //cambia il testo in accendi
+        }
     }
 )
